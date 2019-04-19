181baaad749f725ba7e7a91744da6afd8bc5d1f3
OneBoSS2018 <bob204981@gmail.com>
2019-04-19 20:51:02 +0300

    add validates on models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
 
 	belongs_to :user
+
+	validates :title, :content, :user_id, presence: true
 	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles
+
+  validates :email, presence: true
+  validates_associated :articles
+  validates :password, length: 5..20
+  validates :email, uniqueness: true 
          
 end
