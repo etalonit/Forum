5e9362a1a4330126037d9ff1da16926677a8253c
kachurdima18 <mega.kachur@ukr.net>
2019-06-02 01:55:19 +0300

    Update tests in model User

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     last_name {"lastname"}
     username {"username"}
     about {"lolka"}
+    avatar { Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/noavatar.png'), 'image/png') }
     email {"tester@test.com"}
     password {"tester"}
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,14 +10,36 @@ RSpec.describe User, type: :model do
     it "is valid with valid attributes" do
       expect(subject).to be_valid
     end
+
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
   end
 
   describe "associations" do
     it { should have_many(:articles)}
+
+    it { should have_many(:comments).dependent(:destroy)}
   end
 
-  describe "associations" do
-    it { should have_many(:comments).dependent(:destroy)}
+
+  describe "password validations" do
+
+    it "should require a password" do
+      User.new(subject.update(:password => "", :password_confirmation => "")).
+        should_not be_valid
+    end
+
+    it "should require a matching password confirmation" do
+      User.new(subject.update(:password_confirmation => "invalid")).
+        should_not be_valid
+    end
+
+    it "should reject short passwords" do
+      short = "a" * 5
+      hash = subject.update(:password => short, :password_confirmation => short)
+      User.new(hash).should_not be_valid
+    end
+
   end
 
 end
