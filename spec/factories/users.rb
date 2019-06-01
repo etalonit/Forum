5d47fd32d4a9fb50b34bd2507df724126b0e25a9
kachurdima18 <mega.kachur@ukr.net>
2019-06-02 01:17:10 +0300

    Test User model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-  
+    first_name {"firstname"}
+    last_name {"lastname"}
+    username {"username"}
+    about {"lolka"}
+    email {"tester@test.com"}
+    password {"tester"}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,22 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  subject{
+     build(:user)
+  }
+
+  describe "validation" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+  end
+
+  describe "associations" do
+    it { should have_many(:articles)}
+  end
+
+  describe "associations" do
+    it { should have_many(:comments).dependent(:destroy)}
+  end
+
 end
