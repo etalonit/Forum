3b6b026ca04bd4dd5687df859f9d78a423663b1d
kachurdima18 <mega.kachur@ukr.net>
2019-06-02 11:52:47 +0300

    Tests for model Article

---
+++ spec/factories/articles.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :article do
+    title {"title"}
+    content {"content"}
+    image { Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/noavatar.png'), 'image/png')}
+    #audio { Rack::Test::UploadedFile.new(Rails.root.join('/app/assets/songs/warofchange.mp3'), 'audio/mp3')}
+  end
+end
---
+++ spec/models/article_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+
+  let!(:user){
+    create(:user)
+  }
+
+  subject{
+     build(:article, user: user)
+  }
+
+  context "validation" do
+
+    it { should validate_presence_of(:title) }
+
+    it { should validate_presence_of(:content) }
+
+    it { should validate_presence_of(:user_id) }
+
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "should not be valid without title" do
+       subject.title = nil
+       expect(subject).to_not be_valid
+    end
+
+    it "should not be valid without content" do
+       subject.content = nil
+       expect(subject).to_not be_valid
+    end
+  end
+
+  context "association" do
+    it { should belong_to(:user) }
+
+    it { should have_many(:comments).dependent(:destroy)}
+  end
+
+end
