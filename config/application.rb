1a871b13472b2b68f6da51f93575772c819974a6
byVasko <vasyl101710@gmail.com>
2019-06-14 17:21:16 +0300

    adding a language switch and edit navbar

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Forum
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Kyiv'
+    config.i18n.available_locales = [:uk, :en]
+    config.i18n.default_locale = :uk
   end
 end
