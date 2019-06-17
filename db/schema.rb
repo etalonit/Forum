cf9a34338b551192f67baff97878bfe3b998baed
OneBoSS2018 <bob204981@gmail.com>
2019-06-17 17:51:48 +0300

    delete bool admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_205927) do
+ActiveRecord::Schema.define(version: 2019_06_14_195741) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -82,7 +82,6 @@ ActiveRecord::Schema.define(version: 2019_06_14_205927) do
     t.date "birthday"
     t.string "avatar"
     t.string "slug"
-    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
